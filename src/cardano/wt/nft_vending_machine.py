thaddeusdiamond/cardano-nft-vending-machine

commit 3b84bc7df5a3ed5dd9965e35d077a2fcde7bb0a4

Mint: Remove the concept of "rebates" and directly calculate min-ada-val
The prior commit changed the rebate to being subtracted out of the
mint's gross profits rather than being supplied by the user.  Therefore,
we need to be much more cost conscious than merely sending back the user
a lump sum.

To calculate the rebate, we use the "min-ada-value" formula found in the
Cardano documentation here:
https://docs.cardano.org/native-tokens/minimum-ada-value-requirement

(Special thank you to mantis for his calculator to tell us if we were on
the right track: https://mantis.functionally.io/how-to/min-ada-value/).

Note that this is a bit conservative in that it uses a "coin size" of 0.
In future eras, this will be changed to 2, which will slightly lower
the minimum ada required per transaction.

[ Documentation: README.md to note the changed API/removed params ]
[ Testing: Manual QA with 1 and 6 token mints at a time ]

pyproject.toml
@@ -4,7 +4,7 @@ build-backend = "setuptools.build_meta"
 
 [project]
 name = "cardano-nft-vending-machine"
-version = "0.3.0-beta4"
+version = "0.3.0-beta6"
 
 description = "Library to perform NFT mints automatically on the Cardano blockchain"
 readme = "README.md"

src/cardano/wt/nft_vending_machine.py
@@ -8,10 +8,12 @@ import time
 import traceback
 
 from cardano.wt.cardano_cli import CardanoCli
+from cardano.wt.mint import Mint
 from cardano.wt.utxo import Utxo
 
 class NftVendingMachine(object):
 
+    __SINGLE_POLICY = 1
     __WITNESS_COUNT = 2
 
     def __get_donation_addr(mainnet):
@@ -42,6 +44,11 @@ class NftVendingMachine(object):
             policy_json = json.load(metadata_filehandle)['721'][self.mint.policy]
             names = policy_json.keys()
             return [name.encode('UTF-8').hex() for name in names]
+
+    def __get_nft_names_from(self, metadata_file):
+        with open(metadata_file, 'r') as metadata_filehandle:
+            policy_json = json.load(metadata_filehandle)['721'][self.mint.policy]
+            return policy_json.keys()
 
     def __lock_and_merge(self, available_mints, num_mints, output_dir, locked_subdir, metadata_subdir, txn_id):
         combined_nft_metadata = {}
@@ -77,21 +84,28 @@ class NftVendingMachine(object):
         num_mints_requested = math.floor(lovelace_bal.lovelace / self.mint.price)
         num_mints = min(self.single_vend_max, len(available_mints), num_mints_requested)
         gross_profit = num_mints * self.mint.price
-        net_profit = gross_profit - self.mint.donation - self.mint.rebate 
-        print(f"Beginning to mint {num_mints} NFTs to send to address {input_addr} (rebate: {self.mint.rebate})")
 
+        print(f"Beginning to mint {num_mints} NFTs to send to address {input_addr}")
         txn_id = int(time.time())
         nft_metadata_file = self.__lock_and_merge(available_mints, num_mints, output_dir, locked_subdir, metadata_subdir, txn_id)
         nft_names = self.__generate_nft_names_from(nft_metadata_file)
+
+        total_name_chars = sum([len(name) for name in self.__get_nft_names_from(nft_metadata_file)])
+        user_rebate = Mint.RebateCalculator.calculateRebateFor(NftVendingMachine.__SINGLE_POLICY, num_mints, total_name_chars)
+        net_profit = gross_profit - self.mint.donation - user_rebate 
+        if (net_profit < Utxo.MIN_UTXO_VALUE):
+            raise ValueError(f"Rebate of {user_rebate} would leave too small profit of {net_profit}")
+        print(f"Minimum rebate to user is {user_rebate}, net profit to vault is {net_profit}")
+
         tx_ins = [f"--tx-in {mint_req.hash}#{mint_req.ix}"]
-        tx_outs = self.__get_tx_out_args(input_addr, self.mint.rebate, nft_names, net_profit, self.mint.donation)
+        tx_outs = self.__get_tx_out_args(input_addr, user_rebate, nft_names, net_profit, self.mint.donation)
         mint_build_tmp = self.cardano_cli.build_raw_mint_txn(output_dir, txn_id, tx_ins, tx_outs, 0, nft_metadata_file, self.mint, nft_names)
 
         tx_in_count = len(tx_ins)
         tx_out_count = len([tx_out for tx_out in tx_outs if tx_out])
         fee = self.cardano_cli.calculate_min_fee(mint_build_tmp, tx_in_count, tx_out_count, NftVendingMachine.__WITNESS_COUNT)
 
-        tx_outs = self.__get_tx_out_args(input_addr, self.mint.rebate, nft_names, net_profit - fee, self.mint.donation)
+        tx_outs = self.__get_tx_out_args(input_addr, user_rebate, nft_names, net_profit - fee, self.mint.donation)
         mint_build = self.cardano_cli.build_raw_mint_txn(output_dir, txn_id, tx_ins, tx_outs, fee, nft_metadata_file, self.mint, nft_names)
         mint_signed = self.cardano_cli.sign_txn([self.payment_sign_key, self.mint.sign_key], mint_build)
         self.blockfrost_api.submit_txn(mint_signed)

main.py
@@ -88,7 +88,6 @@ def get_parser():
     parser.add_argument('--payment-sign-key', required=True, help='Location on disk of wallet signing keys for payment landing zone')
     parser.add_argument('--profit-addr', required=True, help='Cardano address where mint profits should be taken (NOTE: HARDWARE/LEDGER RECOMMENDED)')
     parser.add_argument('--mint-price', type=int, required=True, help='Price in lovelace that is being charged for each NFT')
-    parser.add_argument('--mint-rebate', type=int, required=True, help='Amount user expects to receive back (gross of fees)')
     parser.add_argument('--mint-policy', required=True, help='Policy ID of the mint being performed')
     parser.add_argument('--mint-script', required=True, help='Local path of scripting file for mint')
     parser.add_argument('--mint-sign-key', required=True, help='Location on disk of signing keys used for the mint')
@@ -109,11 +108,14 @@ if __name__ == "__main__":
     ensure_output_dirs_made(_args.output_dir)
 
     _donation_amt = get_donation_amt(_args.no_donation)
-    _mint = Mint(_args.mint_policy, _args.mint_price, _args.mint_rebate, _donation_amt, _args.metadata_dir, _args.mint_script, _args.mint_sign_key)
+    _mint = Mint(_args.mint_policy, _args.mint_price, _donation_amt, _args.metadata_dir, _args.mint_script, _args.mint_sign_key)
 
     _blockfrost_api = BlockfrostApi(_args.blockfrost_project, mainnet=_args.mainnet)
 
-    _protocol_params = rewritten_protocol_params(_blockfrost_api.get_protocol_parameters(), _args.output_dir)
+    _blockfrost_protocol_params = _blockfrost_api.get_protocol_parameters()
+    _protocol_params = rewritten_protocol_params(_blockfrost_protocol_params, _args.output_dir)
+    max_txn_fee = (_blockfrost_protocol_params['min_fee_a'] * _blockfrost_protocol_params['max_tx_size']) + _blockfrost_protocol_params['min_fee_b']
+    print(f"Max txn fee is a * size(tx) + b: {max_txn_fee}");
     _cardano_cli = CardanoCli(mainnet=_args.mainnet, protocol_params=_protocol_params)
 
     _nft_vending_machine = NftVendingMachine(

src/cardano/wt/mint.py
@@ -1,9 +1,28 @@
 import json
+import math
+
+from cardano.wt.utxo import Utxo
 
 """
 Representation of the current minting process.
 """
 class Mint(object):
+
+    class RebateCalculator(object):
+        __COIN_SIZE = 0.0               # Will change in next era to slightly lower fees
+        __PIDSIZE = 28.0
+        __UTXO_SIZE_WITHOUT_VAL = 27.0
+
+        __ADA_ONLY_UTXO_SIZE = __COIN_SIZE + __UTXO_SIZE_WITHOUT_VAL
+        __UTXO_BASE_RATIO = math.ceil(Utxo.MIN_UTXO_VALUE / __ADA_ONLY_UTXO_SIZE)
+
+        def calculateRebateFor(num_policies, num_assets, total_name_chars):
+            asset_words = math.ceil(((num_assets * 12.0) + (total_name_chars) + (num_policies * Mint.RebateCalculator.__PIDSIZE)) / 8.0)
+            utxo_native_token_multiplier = Mint.RebateCalculator.__UTXO_SIZE_WITHOUT_VAL + (6 + asset_words)
+            return int(Mint.RebateCalculator.__UTXO_BASE_RATIO * utxo_native_token_multiplier)
+        
+        def __init__(self):
+            raise ValueError('Mint rebate calculator is meant to be used as a static class only')
 
     def __read_validator(validation, key, script):
         with open(script, 'r') as script_file:
@@ -14,10 +33,9 @@ class Mint(object):
                     return validator[key]
         return None
 
-    def __init__(self, policy, price, rebate, donation, nfts_dir, script, sign_key):
+    def __init__(self, policy, price, donation, nfts_dir, script, sign_key):
         self.policy = policy
         self.price = price
-        self.rebate = rebate
         self.donation = donation
         self.nfts_dir = nfts_dir
         self.script = script

src/cardano/wt/utxo.py
@@ -5,6 +5,8 @@ Simple UTXO object to strengthen the type of CLI-returned strings
 """
 class Utxo(object):
 
+    MIN_UTXO_VALUE = 1000000
+
     class Balance(object):
         LOVELACE_POLICY = 'lovelace'
 

commit 08c08eb97401beb2dfa43f561961b36722661f24

NftVendingMachine: Allow for inexact change to be returned
Commit 3b84bc7 introduced a bug whereby the user could submit a transaction
that was not a perfect multiple of the mint price and the transaction
would fail.  We need to correct that so if someone does math incorrectly
but sends directly to our minting address, we provide change back
gracefully.

[ Documentation: None ]
[ Testing: Manual transactions w/ and w/o exact change ]

pyproject.toml
@@ -4,7 +4,7 @@ build-backend = "setuptools.build_meta"
 
 [project]
 name = "cardano-nft-vending-machine"
-version = "0.3.0-beta6"
+version = "0.3.0-beta7"
 
 description = "Library to perform NFT mints automatically on the Cardano blockchain"
 readme = "README.md"

src/cardano/wt/nft_vending_machine.py
@@ -84,6 +84,7 @@ class NftVendingMachine(object):
         num_mints_requested = math.floor(lovelace_bal.lovelace / self.mint.price)
         num_mints = min(self.single_vend_max, len(available_mints), num_mints_requested)
         gross_profit = num_mints * self.mint.price
+        change = lovelace_bal.lovelace - gross_profit
 
         print(f"Beginning to mint {num_mints} NFTs to send to address {input_addr}")
         txn_id = int(time.time())
@@ -98,14 +99,14 @@ class NftVendingMachine(object):
         print(f"Minimum rebate to user is {user_rebate}, net profit to vault is {net_profit}")
 
         tx_ins = [f"--tx-in {mint_req.hash}#{mint_req.ix}"]
-        tx_outs = self.__get_tx_out_args(input_addr, user_rebate, nft_names, net_profit, self.mint.donation)
+        tx_outs = self.__get_tx_out_args(input_addr, user_rebate + change, nft_names, net_profit, self.mint.donation)
         mint_build_tmp = self.cardano_cli.build_raw_mint_txn(output_dir, txn_id, tx_ins, tx_outs, 0, nft_metadata_file, self.mint, nft_names)
 
         tx_in_count = len(tx_ins)
         tx_out_count = len([tx_out for tx_out in tx_outs if tx_out])
         fee = self.cardano_cli.calculate_min_fee(mint_build_tmp, tx_in_count, tx_out_count, NftVendingMachine.__WITNESS_COUNT)
 
-        tx_outs = self.__get_tx_out_args(input_addr, user_rebate, nft_names, net_profit - fee, self.mint.donation)
+        tx_outs = self.__get_tx_out_args(input_addr, user_rebate + change, nft_names, net_profit - fee, self.mint.donation)
         mint_build = self.cardano_cli.build_raw_mint_txn(output_dir, txn_id, tx_ins, tx_outs, fee, nft_metadata_file, self.mint, nft_names)
         mint_signed = self.cardano_cli.sign_txn([self.payment_sign_key, self.mint.sign_key], mint_build)
         self.blockfrost_api.submit_txn(mint_signed)