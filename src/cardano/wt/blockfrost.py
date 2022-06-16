thaddeusdiamond/cardano-nft-vending-machine

commit a0dcdfbf2847d40fd5cf555d22d66afbf140ff5a

Blockfrost: Two API Hardening Issues Fixed
1. Sometimes the UTXO has appeared in our address but we don't actually
   get the input address because Blockfrost is backed up.  This change
   adds an optional retry loop
2. Lots of times there will be more than one input address for a wallet.
   Shift so that we just pick the first one in that case

[ Documentation: None ]
[ Testing: Live mint! ]

pyproject.toml
@@ -4,7 +4,7 @@ build-backend = "setuptools.build_meta"
 
 [project]
 name = "cardano-nft-vending-machine"
-version = "0.3.0-beta8"
+version = "0.3.0-beta9"
 
 description = "Library to perform NFT mints automatically on the Cardano blockchain"
 readme = "README.md"

src/cardano/wt/blockfrost.py
@@ -12,7 +12,9 @@ Repreentation of the Blockfrost web API used in retrieving metadata about txn i/
 class BlockfrostApi(object):
 
     _API_CALLS_PER_SEC = 10
+    _BACKOFF_SEC = 5
     _BURST_TXN_PER_SEC = 10
+    _MAX_RETRIES = 9
     _MAX_BURST = 500
     _UTXO_LIST_LIMIT = 100
 
@@ -47,13 +49,22 @@ class BlockfrostApi(object):
     def __get_api_base(self):
         return f"https://cardano-{'mainnet' if self.mainnet else 'testnet'}.blockfrost.io/api/v0"
 
-    def __call_get_api(self, resource):
+    def __call_get_api(self, resource, max_retries=0):
         self.__account_for_rate_limit()
-        api_resp = requests.get(f"{self.__get_api_base()}/{resource}", headers={'project_id': self.project})
-        print(f"Blockfrost API: {resource} ({api_resp.status_code})")
-        api_resp.raise_for_status()
-        print(api_resp.json())
-        return api_resp.json()
+        retries = 0
+        while True:
+            try:
+                pi_resp = requests.get(f"{self.__get_api_base()}/{resource}", headers={'project_id': self.project})
+                print(f"Blockfrost API: {resource} ({api_resp.status_code})")
+                api_resp.raise_for_status()
+                print(api_resp.json())
+                return api_resp.json()
+            except requests.exceptions.HTTPError as e:
+                if retries < max_retries:
+                    retries += 1
+                    time.sleep(BlockfrostApi._BACKOFF_SEC)
+                else:
+                    raise e
 
     def __call_post_api(self, content_type, resource, data):
         self.__account_for_rate_limit()
@@ -63,12 +74,11 @@ class BlockfrostApi(object):
         api_resp.raise_for_status()
         return api_resp.json()
 
-
     def get_input_address(self, txn_hash):
-        utxo_metadata = self.__call_get_api(f"txs/{txn_hash}/utxos")
+        utxo_metadata = self.__call_get_api(f"txs/{txn_hash}/utxos", max_retries=BlockfrostApi._MAX_RETRIES)
         utxo_inputs = set([utxo_input['address'] for utxo_input in utxo_metadata['inputs']])
-        if len(utxo_inputs) != 1:
-            raise ValueError(f"Txn hash {txn_hash} came from != 1 addresses({utxo_inputs}), aborting...")
+        if len(utxo_inputs) < 1:
+            raise ValueError(f"Txn hash {txn_hash} has no valid addresses({utxo_inputs}), aborting...")
         return utxo_inputs.pop()
 
     def get_utxos(self, address, exclusions):

commit 03f4f44f1e104dd7666a4cf0640691a03814b54e

Fix bug introduced by naming in a0dcdfb

pyproject.toml
@@ -4,7 +4,7 @@ build-backend = "setuptools.build_meta"
 
 [project]
 name = "cardano-nft-vending-machine"
-version = "0.3.0-beta9"
+version = "0.3.0-beta10"
 
 description = "Library to perform NFT mints automatically on the Cardano blockchain"
 readme = "README.md"

src/cardano/wt/blockfrost.py
@@ -54,7 +54,7 @@ class BlockfrostApi(object):
         retries = 0
         while True:
             try:
-                pi_resp = requests.get(f"{self.__get_api_base()}/{resource}", headers={'project_id': self.project})
+                api_resp = requests.get(f"{self.__get_api_base()}/{resource}", headers={'project_id': self.project})
                 print(f"Blockfrost API: {resource} ({api_resp.status_code})")
                 api_resp.raise_for_status()
                 print(api_resp.json())