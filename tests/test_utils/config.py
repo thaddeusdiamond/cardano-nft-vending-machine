52bd3a402410357add00046ce19f2263332fc620
Thaddeus Diamond <thaddeus.diamond@gmail.com>
2023-05-27 18:54:15 -0500

    Testing: Allow for alternative sources of funding (e.g., for parallelizing testing)

--- tests/test_utils/config.py
+++ tests/test_utils/config.py
@@ -5,6 +5,9 @@ from test_utils.keys import KeyPair
 
 from test_utils.blockfrost import get_network_magic
 
+FUNDER_NAME = os.environ['FUNDER'] if 'FUNDER' in os.environ else 'funder'
+
 def get_funder_address(request):
     secrets_dir = os.path.join(os.path.dirname(request.fspath), 'secrets')
-    return Address.existing(KeyPair.existing(secrets_dir, 'funder'), get_network_magic())
+    print(f"Using funder '{FUNDER_NAME}'")
+    return Address.existing(KeyPair.existing(secrets_dir, FUNDER_NAME), get_network_magic())
